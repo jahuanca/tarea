1081a003b4609efefcc7a7b74dcc6e79e40382ac
jahuanca <joan.huanca19@gmail.com>
2021-09-23 00:27:47 -0500

    sincronizacion

--- lib/core/strings.dart
+++ lib/core/strings.dart
@@ -1,3 +1,4 @@
 final String serverUrlCorta='10.0.2.2:3000';
+//final String serverUrlCorta='40.88.149.7/node/node';
 final String serverUrl='http://$serverUrlCorta';
 final String rImages='assets/images/';
